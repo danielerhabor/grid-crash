2f564729c2279f3be78bf2e40fe532bef8d6cceb
Daniel Erhabor <danielnerhabor@gmail.com>
2022-06-04 13:14:40 -0400

    Used `grid-template-areas` and `grid-area` to style a simple layout.

--- src/AppTwo.tsx
+++ src/AppTwo.tsx
@@ -2,13 +2,13 @@ import styles from './AppTwo.module.css';
 
 const AppTwo = () => {
   return (
-    <>
+    <div className={styles.container}>
       <header>Header</header>
       <main>Content</main>
       <nav>Navigation</nav>
       <aside>Sidebar</aside>
       <footer>Footer</footer>
-    </>
+    </div>
   );
 };
 
