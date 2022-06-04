89b52264a002e06e036dd648f6135c1d851d1b4c
Daniel Erhabor <danielnerhabor@gmail.com>
2022-06-03 21:49:42 -0400

    used shorthand for `gap`, used `grid-auto-rows` to set the height of all the grid rows. Used `minmax` for bounding the minimum and maximum height of all the grid rows. used `grid-template-rows` to specify the height of each grid row

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import styles from './App.module.css';
 const App = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.item}>Item 1</div>
+      <div className={styles.item}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ratione
+        modi tempore ad voluptate ab laboriosam dolor ea eius numquam
+        consequuntur iusto nisi nostrum rerum, alias, magni repellendus corrupti
+        laborum.
+      </div>
       <div className={styles.item}>Item 2</div>
       <div className={styles.item}>Item 3</div>
       <div className={styles.item}>Item 4</div>
