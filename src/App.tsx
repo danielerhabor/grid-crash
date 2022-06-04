0630cd88a3347dce40e89feb46447c1e76fd8136
Daniel Erhabor <danielnerhabor@gmail.com>
2022-06-04 13:40:39 -0400

    Reinitialized Source Code for the Testimonials Project Using Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
-import styles from './App.module.css';
-
 const App = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.item}>Item 1</div>
-      <div className={styles.item}>Item 2</div>
-      <div className={styles.item}>Item 3</div>
-      <div className={styles.item}>Item 4</div>
-      <div className={styles.item}>Item 5</div>
-      <div className={styles.item}>Item 6</div>
-      <div className={styles.item}>Item 7</div>
-      <div className={styles.item}>Item 8</div>
-      <div className={styles.item}>Item 9</div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// import App from './App';
-import AppTwo from './AppTwo';
+import App from './App';
 
 import './index.css';
 
@@ -11,7 +10,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <AppTwo />
+    <App />
   </React.StrictMode>
 );
